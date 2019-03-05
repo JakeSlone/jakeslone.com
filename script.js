36569645788a70ca7ede678047e21e8e0b379f60
JakeSlone <giraffe.jake@gmail.com>
2019-03-04 22:07:01 -0700

    c

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ toggler.addEventListener('click', () => {
     toggler.classList.toggle('active');
     menu.classList.toggle('active');
 });
-// Down Arrow
+// Down Arrow 1
 $('scrollDown1').click(function() {
     var offset = 20;
 
@@ -14,3 +14,19 @@ $('scrollDown1').click(function() {
         scrollTop: $('#down1').offset().top + offset
     }, 1000);
 });
+// About Image
+let bioImg = document.getElementById('bioImg');
+let img = 0;
+let imgList = [
+    "./img/code1.jpg",
+    "./img/code2.png",
+    "./img/code3.jpg"
+]
+setInterval(function() {
+    img += 1;
+    if (img > bioImg.length - 1) {
+        img = 0;
+    }
+    bioImg.style.backgroundImage = "url('" + [img] + "')";
+},1000);
+

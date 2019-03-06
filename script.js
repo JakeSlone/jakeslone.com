17a8e359b1eaa94e5d17dbc6b45c213097a07c4e
JakeSlone <giraffe.jake@gmail.com>
2019-03-05 21:30:22 -0700

    c

--- script.js
+++ script.js
@@ -28,5 +28,13 @@ setInterval(function() {
         img = 0;
     }
     bioImg.style.backgroundImage = "url('" + imgList[img] + "')";
-},1000);
+},3000);
+// Down Arrow 2
+$(document).ready(function () {
+    $("#scrollDown2").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#projects").offset().top
+        }, 3000);
+    });
+});
 
